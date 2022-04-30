8f854e26e5a638f382732133362454506a53cda1
Kennedy <kennedy@dyn-160-39-167-50.dyn.columbia.edu>
2022-04-30 13:58:20 -0400

    updated with transconv run

--- modelSumm.py
+++ modelSumm.py
@@ -4,4 +4,4 @@ from torchinfo import summary
 if __name__ == '__main__':
 	
 	#print(ConvTransNet())
-	summary(ConvTransNet(), input_size = (1, 5, 200))
+	summary(BetterConvNet(), input_size = (1, 5, 200))
